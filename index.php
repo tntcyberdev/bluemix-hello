55b858cd2aebb9e38f7854865108871092dc7f65
Tom Tran <webtomme@gmail.com>
2015-07-19 00:21:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,22 +3,19 @@
 	<title>Hello world!</title>
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
-	body {
+   	  body {
 		background-color: white;
 		text-align: center;
 		padding: 50px;
 		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
-	}
-	#logo {
-		margin-bottom: 40px;
-	}
+  	  }
 	</style>
 </head>
 <body>
-	<img id="logo" src="www/logo.jpeg" />
-	<h1>Hello Docker Vietnam 2015</h1>
-	<p>My name is Tom. Today I show you Bluemix, a PaaS solution by IBM.</p>
-
-	<?php print "Today: " . date('dS F, Y'); ?>
+	<img id="logo" src="www/logo.jpeg">
+	<h1>Hello Docker Vietnam 2015</h1> 
+  	<p><?php print "Today: " . date('dS F, Y'); ?></p>
+	<h3>Hi, my name is Tom. Today I show you Bluemix, a PaaS solution by IBM.</h3><br>
+  	<img src="http://cs.smu.ca/bluemixhackathon/img/IBMBlueMix.jpg" height="100px">
 </body>
 </html>
