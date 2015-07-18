d8867bf6a691f2d2125fe0efca6772e4f3e7c20a
Tom Tran <webtomme@gmail.com>
2015-07-19 00:27:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
 	<img id="logo" src="www/logo.jpeg">
 	<h1>Hello Docker Vietnam 2015</h1> 
   	<p><?php print "Today: " . date('dS F, Y'); ?></p>
-	<h3>Hi, my name is Tom Tran. Today I'll demo Bluemix, a PaaS solution by IBM.</h3><br>
+	<h3>Hi, my name is Tom Tran. Today I'll demo Bluemix, a PaaS solution by IBM.</h3>
 	<p>You can tweet me @webtomme.</p>
   	<img src="http://cs.smu.ca/bluemixhackathon/img/IBMBlueMix.jpg" height="100px">
 </body>
